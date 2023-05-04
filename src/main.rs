6b6662812694aadf990fe20311c511fca8ad496e
Ahmed Abo Elfadle <arabea169@gmail.com>
2023-05-04 22:35:28 +0200

    Added Accept Number from user Feature

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess The Number!");
+    println!("Please Enter Your Guess...");
+
+    let mut guess = String::new();
+
+    // Accept input from user and bind with the guess variable
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line!");
+
+    println!("You Guessed the Number: {guess}");
+
 }
